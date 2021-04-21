e4e60575c7c80a623de6397e5bba7b337857088f
Alex <alex.h.pachas@gmail.com>
2021-04-20 23:33:30 -0500

    avance curso planes

--- app/Http/Livewire/Modulos/Cursoplanes/CursoPlanesIndex.php
+++ app/Http/Livewire/Modulos/Cursoplanes/CursoPlanesIndex.php
@@ -4,21 +4,28 @@ namespace App\Http\Livewire\Modulos\CursoPlanes;
 
 use App\Models\Ciclo;
 use App\Models\Curso;
+use App\Models\Facultade;
 use App\Models\PlanEstudio;
+use App\Models\Programa;
 use Livewire\Component;
 
 class CursoPlanesIndex extends Component
 {
+
+    
     public $buscador="";
     public $campo="id";
     public $direccion="desc";
+    public $idfacultad;
+    public $idprograma;
     
     public function render()
     {
-        $planEstudios = PlanEstudio::all();
-        $ciclos = Ciclo::all();
-        $cursos = Curso::all();
-        return view('livewire.modulos.curso-planes.curso-planes-index',compact('planEstudios','ciclos','cursos'));
+        $facultades = Facultade::all();   
+        $programas = Programa::where('facultade_id',$this->idfacultad)->get();
+        $planEstudios = PlanEstudio::where('programa_id',$this->idprograma)->get();
+             
+        return view('livewire.modulos.curso-planes.curso-planes-index',compact('facultades','programas','planEstudios'));
     }
 
     public function index(){
